05818070a4769334ede4bc970d76b632b368029c
Maynor de Leon <maynoralex@gmail.com>
2025-03-08 09:27:05 +0100

    initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule, EffectsRootModule, provideEffects } from '@ngrx/effects';
+import { BookReducer } from './books/book.reducer';
+import { AppState } from './app.state';
+import { BookEffects } from './books/book.effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideStore({ book: BookReducer }), 
+    provideEffects([BookEffects]),
+  ]
 };
